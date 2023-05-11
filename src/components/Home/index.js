3338738aabeafb8f5188e5e19076464ec49ba611
vaishali199807 <52350197+vaishali199807@users.noreply.github.com>
2023-05-11 22:41:17 +0530

    Add files via upload

---
+++ src/components/Home/index.js
@@ -0,0 +1,27 @@
+import LogoTitle from '../../assets/images/logo-s2.png'
+import {Link} from 'react-router-dom'
+import './index.scss'
+
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm
+                <img src={LogoTitle} alt="dev"/>
+                aishali
+                <br/>
+                web developer
+                </h1>
+                <h2>
+                    Frontend Developer / Javascript / React
+                </h2>
+                <Link to="/contact" className='flat-button'>
+                    CONTACT ME
+                </Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,16 +1,17 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+// import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faSkype} from '@fortawesome/free-brands-svg-icons'
 
 const SideBar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
       <img src={LogoS} alt="main-logo" />
 
-      <img className="sub-logo" src={LogoSubtitle} alt="sub-logo" />
+      {/* <img className="sub-logo" src={LogoSubtitle} alt="sub-logo" /> */}
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
@@ -35,6 +36,35 @@ const SideBar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+        <li>
+            <a
+                href='linkedin.com'
+                target='_blank'
+                rel='noreferrer'
+            >
+                <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'></FontAwesomeIcon>
+            </a>
+        </li>
+        <li>
+            <a
+                href='linkedin.com'
+                target='_blank'
+                rel='noreferrer'
+            >
+                <FontAwesomeIcon icon={faGithub} color='#4d4d4e'></FontAwesomeIcon>
+            </a>
+        </li>
+        <li>
+            <a
+                href='linkedin.com'
+                target='_blank'
+                rel='noreferrer'
+            >
+                <FontAwesomeIcon icon={faSkype} color='#4d4d4e'></FontAwesomeIcon>
+            </a>
+        </li>
+    </ul>
   </div>
 )
 
